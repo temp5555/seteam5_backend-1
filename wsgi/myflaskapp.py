2ba55a3db71a17ba8a216ea24923e82d445f9a16
temp5555 <tempfire57@gmail.com>
2016-03-19 15:45:14 +0530

    Update myflaskapp.py

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world!', 201
+    return 'Hello world!', 200
 
 
 @app.route('/userinfo/', methods=['POST'])
@@ -36,23 +36,21 @@ def get_userinfo(phonenumber):
 def admin_login():
     return render_template("admin_login.html")
 
+@app.route('/map.html')
+def map():
+    return render_template("map.html")
 
-@app.route('/admin_student_status.html', methods=['GET'])
+
+@app.route('/admin_student_status.html')
 def admin_student_status():
-    pending = database_driver.get_students_info({"status": {"$exists": False}})
-    approoved = database_driver.get_students_info({"status": "aproove"})
-    declined = database_driver.get_students_info({"status": "decline"})
-    return render_template('admin_student_status.html', pending=pending,
-                           approoved=approoved, declined=declined)
+    pending = database_driver.get_students_info()
+    declined = database_driver.get_students_info()
+    return render_template('admin_student_status.html', pending=pending,declined=declined)
 
 
-@app.route('/admin_student_status.html', methods=['POST'])
-def admin_student_status_update():
-    update = {"phonenumber": request.form['phonenumber'],
-              "status": request.form['status'],
-              }
-    database_driver.update_student_info(update)
-    return '', 200
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
