b45c6175aee0b70d8c40f3200cef303f6833ee47
temp5555 <tempfire57@gmail.com>
2016-04-14 20:04:16 +0530

    Update cloud_messaging.py

--- wsgi/cloud_messaging.py
+++ wsgi/cloud_messaging.py
@@ -1,5 +1,6 @@
 from gcm import GCM
 import database_driver
+from twilio.rest import TwilioRestClient
 
 
 def send_message(from_, to_, message):
@@ -10,5 +11,14 @@ def send_message(from_, to_, message):
     gcm.json_request(registration_ids, data)
 
 
-def send_sms(to_, messsage):
-    pass
+def send_sms(myTwilioNumber,myCellPhone,message):
+	accountSID = 'AC9ca04bf819393f32f29cc73f007b2deb'
+	authToken = 'b791d0d1391c8f24c1a48df1d8755a65'
+	twilioCli = TwilioRestClient(accountSID, authToken)
+	message = twilioCli.messages.create(body=message, from_=myTwilioNumber, to=myCellPhone)
+
+
+#remove the comments below to run a test
+
+#msg = "Running test"
+#send_sms('+12016902826','+918050642408',msg)
